3625959ce12f90b7a3be3b81d4358859738cddb2
Towry Wang <towry@users.noreply.github.com>
2024-07-24 22:05:45 +0800

    wip: <😎>[skip ci] Wed Jul 24 22:05:45 CST 2024

--- npmpink-core/src/iterators.rs
+++ npmpink-core/src/iterators.rs
@@ -4,7 +4,6 @@ pub fn packages_jsons(workspaces: Vec<Workspace>) -> Vec<String> {
     workspaces
         .iter()
         .flat_map(|w| w.walk_package_jsons())
-        .flatten()
         .map(|p| p.to_str().unwrap().to_string())
         .collect()
 }
@@ -13,7 +12,6 @@ pub fn packages_from_source(source: &Source) -> Vec<Package> {
     let workspace = Workspace::init_from_dir(source.path.clone());
     workspace
         .walk_package_jsons()
-        .unwrap()
         .map(|p| p.to_str().unwrap().to_string())
         .map(|p| {
             let ws = Workspace::init_from_dir(&p);
